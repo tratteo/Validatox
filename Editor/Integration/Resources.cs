82e820b2a2a43d3c3c1894e191d283775e8d08ed
Matteo Beltrame <bltmtt@gmail.com>
2022-09-03 14:58:27 +0200

    package, naming

--- Editor/Integration/Resources.cs
+++ Editor/Integration/Resources.cs
@@ -5,8 +5,8 @@ namespace Validatox.Editor
 {
     internal class Resources
     {
-        public const string PackageId = "com.trat.validatox";
-        public const string Author = "Trat";
+        public const string PackageId = "com.tratteo.validatox";
+        public const string Author = "tratteo";
         public const string Name = "Validatox";
 
         public static Texture2D Icon() => AssetDatabase.LoadAssetAtPath<Texture2D>($"{GetPackageRoot()}/Editor/Icons/validatox_icon.png");
